b0c12fb98de99fd11b171ee5f626fdc68a27d1fc
Arthur <arakhiyarov@gmail.com>
2022-07-01 19:30:00 +0500

    added view and completed unstake funcs

--- contracts/Staking_Tron.sol
+++ contracts/Staking_Tron.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 error Staking__TransferFailed();
 
-contract StakingTron {
+contract StakingTron is Ownable {
 
     // IERC20 immutable public trxToken;
 
@@ -14,9 +15,9 @@ contract StakingTron {
 
     uint256 public s_totalSupply; // total staked balance
     uint public interest; // percent per munite
-    uint public fee; 
-    uint index = 0;
-    address ownerAddr;
+    uint public fee;
+    uint public ownerFeeBalance; 
+    uint public index = 1;
 
     struct Staker {
         address stakerAddr;
@@ -33,26 +34,28 @@ contract StakingTron {
 
     constructor(uint feeRate, uint interestRate) {
         // trxToken = IERC20(_trxToken);
-        ownerAddr = msg.sender;
         fee = feeRate;
         interest = interestRate;
     }
 
     function stake(uint256 amount, uint timeInMinutes) public payable {
         require(amount != 0, "You cannot stake 0 tokens");
-        s_balances[msg.sender] = s_balances[msg.sender] + amount;
+        s_balances[msg.sender] += amount;
         s_totalSupply += amount;
-        // Probably not the best solution
-        if (bytes32(stakersIndexes[msg.sender]) == "0x0000000000000000000000000000000000000000000000000000000000000000") {
-            Staker storage newStaker;
+
+        if ((stakersIndexes[msg.sender]) == 0) {
+            Staker memory newStaker = Staker(
+                {
+                    stakerAddr: msg.sender,
+                    amountStaked: amount,
+                    stakePeriod: timeInMinutes,
+                    depositStartTime: block.timestamp,
+                    depositFinishTime: block.timestamp + timeInMinutes * 60,
+                    hasStaked: true,
+                    index: index - 1
+                }
+            );
             stakersIndexes[msg.sender] = index;
-            newStaker.stakerAddr = msg.sender;
-            newStaker.amountStaked = amount;
-            newStaker.stakePeriod = timeInMinutes;
-            newStaker.depositStartTime = block.timestamp;
-            newStaker.depositFinishTime = block.timestamp + timeInMinutes * 60;
-            newStaker.hasStaked = true;
-            newStaker.index = index;
             index++;
             stakersList.push(newStaker);
         } else {
@@ -60,7 +63,7 @@ contract StakingTron {
             Staker storage existingStaker = stakersList[currentIndex];
             require(existingStaker.hasStaked == false, 'You have already staked');
             Staker storage existingStakerInList = stakersList[existingStaker.index];
-            existingStakerInList.amountStaked = amount;
+            existingStakerInList.amountStaked += amount;
             existingStaker.stakePeriod = timeInMinutes;
             existingStakerInList.depositStartTime = block.timestamp;
             existingStakerInList.depositFinishTime = block.timestamp + timeInMinutes;
@@ -72,57 +75,72 @@ contract StakingTron {
         // }
     }
 
-    function stakeFor3minutes(uint256 amount) public {
+    function stakeFor3minutes(uint256 amount) public payable {
         stake(amount, 3);
     }
 
-    function stakeFor5minutes(uint256 amount) public {
+    function stakeFor5minutes(uint256 amount) public payable {
         stake(amount, 5);
     }
 
-    function stakeFor10minutes(uint256 amount) public {
+    function stakeFor10minutes(uint256 amount) public payable{
         stake(amount, 10);
     }
 
-    function unstakeAndClaimReward(uint256 amount) public {
+    function unstakeAndClaimReward(uint256 amount) public returns(uint reward){
+
         uint currentIndex = stakersIndexes[msg.sender];
         Staker storage existingStaker = stakersList[currentIndex];
-        require(existingStaker.depositFinishTime < block.timestamp, "Your stake has not finished yet");
-        uint rewardRaw = (existingStaker.amountStaked / 100) * interest * existingStaker.stakePeriod;
-        uint rewardFinal = rewardRaw - (rewardRaw / 100) * fee;
-        uint stakerAmount = existingStaker.amountStaked;
 
-        existingStaker.amountStaked = 0;
+        require(existingStaker.depositFinishTime < block.timestamp, "Your stake has not finished yet");
+        require(existingStaker.amountStaked >= amount, "Can not unstake that much");
+        require(amount > 0, "Can not unstake 0");
+
+        uint rewardRaw = (existingStaker.amountStaked / 100) * interest * existingStaker.stakePeriod;
+        uint ownerFee = (rewardRaw / 100) * fee;
+        uint rewardFinal = rewardRaw - ownerFee;
+        // uint stakerAmount = existingStaker.amountStaked;
+
+        ownerFeeBalance += ownerFee;
+
+
+        existingStaker.amountStaked -= amount;
         existingStaker.stakePeriod = 0;
         existingStaker.depositStartTime = 0;
         existingStaker.depositFinishTime = 0;
         existingStaker.hasStaked = false;
 
-        s_balances[msg.sender] = s_balances[msg.sender] - amount;
+        s_balances[msg.sender] -= amount;
 
-        // bool success = trxToken.transfer(msg.sender, stakerAmount + rewardFinal);
-        // if(!success) {
+        // bool successUnstake = trxToken.transfer(msg.sender, stakerAmount + rewardFinal);
+        // if(!successUnstake) {
+        //     revert Staking__TransferFailed();
+        // }
+
+        // bool successOwnerFee = trxToken.transfer(owner(), ownerFeeBalance);
+        // if(!successOwnerFee) {
         //     revert Staking__TransferFailed();
         // }
 
         return rewardFinal;
-    }
-
-    function claimReward() public {
-        Staker storage existingStaker = stakersMap[msg.sender];
-        existingStaker.hasStaked = false;
-        Staker storage existingStakerInList = stakersList[existingStaker.index];
-        existingStakerInList.hasStaked = false;
-
 
     }
     
     function getStakers() public view returns(Staker[] memory) {
         return stakersList;
-        
     }
 
-    // function getTotalBalance() {
-    //     return s_totalSupply;
-    // }
+    function getTotalBalance() public view returns(uint totalSupply){
+        return s_totalSupply;
+    }
+
+    function setInterestFee(uint newInterest) external onlyOwner returns(uint _interest) {
+        interest = newInterest;
+        return interest;
+    }
+
+    function setFeeRate(uint newFee) external onlyOwner returns(uint _fee) {
+        fee = newFee;
+        return fee;
+    }
 }
